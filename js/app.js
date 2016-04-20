bfcb9de4b519d1c1bc0db0f716beb8872ea84381
Russell Morgan <russell.morgan@gmail.com>
2016-04-20 10:00:06 -0400

    moved close to done and added error checking

--- js/app.js
+++ js/app.js
@@ -6,10 +6,17 @@ $(document).ready(function() {
         $.get( loc, function( data ) {
             $( ".modal" ).html(data).fadeIn();
         })
+        .done(function() {
+            console.log("completed task");
+               //dismiss modals
+                $(".fa-times").click(function() {
+                    console.log("click!!!");
+                    $(".modal").hide();
+                })     
+        })
+        .fail(function() {
+            alert("Sorry, something went wrong with that request. Please try again");
+        })
     })
-    //dismiss modals
-    $(".fa-times").click(function() {
-        console.log("click!!!");
-        $(".modal").hide();
-    })
+
 });
