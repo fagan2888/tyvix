f78b9634fceef9c36eb996bd7c03b222858074ea
Russell Morgan <russell.morgan@gmail.com>
2016-04-20 09:45:48 -0400

    testing 2 files

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $(".link").click(function() {
         var loc = $(this).attr("alt");
         loc = loc+".html";
-        console.log(loc);
         $.get( loc, function( data ) {
             $( ".modal" ).html(data).fadeIn();
         })
