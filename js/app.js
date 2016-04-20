bd1fb23447db8c1f6315fff27e5b684c71615594
Russell Morgan <russell.morgan@gmail.com>
2016-04-20 09:50:04 -0400

    close modal function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+    //show modals
     $(".link").click(function() {
         var loc = $(this).attr("alt");
         loc = loc+".html";
         $.get( loc, function( data ) {
             $( ".modal" ).html(data).fadeIn();
         })
+    })
+    //dismiss modals
+    $(".fa-times").click(function() {
+        $(".modal").fadeOut();
     })
 });
