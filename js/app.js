7f54544c6238376bf17607c31b4ce77dec19cd66
Russell Morgan <russell.morgan@gmail.com>
2016-04-25 14:56:02 -0400

    layout complete

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     //show modals
     $(".star").click(function() {
+        var pos = $(window).scrollTop();
         var loc = $(this).attr("alt");
         loc = loc+".html";
+        $(".scrim").fadeIn(200);
         $.get( loc, function( data ) {
-            $( ".modal" ).html(data).fadeIn(200);
+            $( ".modal" ).html(data).fadeIn(200).css("top",pos);
         })
         .done(function() {
             console.log("completed task");
                //dismiss modals
                 $(".fa-times").click(function() {
-                    $(".modal").hide();
+                    $(".modal, .scrim").hide();
                 })     
         })
         .fail(function() {
